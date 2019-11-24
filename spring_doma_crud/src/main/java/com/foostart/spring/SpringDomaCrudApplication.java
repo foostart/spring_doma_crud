f9e37ec454834e6785df13dd5d9935549f4f0b0a
foostart.com <foostart.com@gmail.com>
2019-11-24 15:57:08 +0700

    Create first project with Spring Boot Core

---
+++ spring_doma_crud/src/main/java/com/foostart/spring/SpringDomaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.foostart.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDomaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDomaCrudApplication.class, args);
+	}
+
+}
